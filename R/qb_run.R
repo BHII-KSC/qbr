af362bc1955aa7d0151fe7f904766972d9ee9a0f
John Erdmann <john.erdmann@keene.edu>
2022-05-12 10:02:14 -0400

    Major overhaul to qb_run which makes it recursively call the API when results are paginated. Output will now always be tibble, no raw JSON option.

--- R/qb_run.R
+++ R/qb_run.R
@@ -40,15 +40,15 @@
 #'     table_id = "bn9d8iesz",
 #'     report_id = "1")
 #'
-#' # Get rows 3 to 10 froma report
+#' # Get rows 3 to 6 from a report
 #' my_tibble <- qb_run(subdomain = "bhi.quickbase.com",
-#'     user_token = paste0("QB-USER-TOKEN ", keyring::key_get("qb_example")),
+#'     user_token = keyring::key_get("qb_example"),
 #'     table_id = "bn9d8iesz",
 #'     report_id = "1",
 #'     skip = 2,
-#'     top = 10)
+#'     top = 3)
 qb_run <- function(subdomain, user_token, table_id, report_id, agent = NULL,
-                   skip = NULL, top = NULL, type_suffix = FALSE, paginate = TRUE) {
+                   skip = 0, top = 0, type_suffix = FALSE, paginate = TRUE) {
 
   # Validate arguments and fix where possible
   if(!stringr::str_detect(user_token, "^QB-USER-TOKEN ")){
@@ -59,21 +59,61 @@ qb_run <- function(subdomain, user_token, table_id, report_id, agent = NULL,
     subdomain <- stringr::str_c(subdomain, ".quickbase.com")
   }
 
-  if(!is.null(skip)){
-    skip = stringr::str_c("&skip=", as.character(skip))
+  # Call API
+  print("Calling Quickbase API")
+  data_text <- run_report(subdomain, user_token, table_id, report_id, agent, skip, top, paginate)
+
+  print("Cleaning extracted data")
+
+  # Prepare field labels for renaming values object
+  data_fields <- data_text[[2]] %>%
+    dplyr::mutate(id = as.character(id),
+                  label_type = stringr::str_c(label, type, sep =".")) %>%
+    dplyr::arrange(id)
+
+  # Multi-dimensional fields to drop by suffix
+  drop_me <- c(".version", ".id", ".name", ".userName")
+
+  tryCatch(
+    data_clean <- data_text[[1]] %>%
+      dplyr::select(-dplyr::contains(drop_me)) %>%
+      dplyr::rename_with( ~ data_fields$label_type) %>%
+      dplyr::mutate(dplyr::across(dplyr::matches("[.]multitext"), ~ lapply(., paste, collapse = "; "))) %>%
+      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ purrr::map(., "email"))) %>%
+      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ lapply(., paste, collapse = "; "))) %>%
+      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ unlist(.))),
+    error = function(e)
+      stop("The report data could not be parsed.
+           Try removing fields with complex data types."))
+
+  # Reduce # records to 'top' (only applies to paginated returns)
+  if(nrow(data_clean) > top & top > 0){
+    data_clean <- data_clean[1:top, ]
   }
 
-  if(!is.null(top)){
-    top = stringr::str_c("&top=", as.character(top))
+  if(type_suffix == FALSE){
+    data_clean <- data_clean %>%
+      dplyr::rename_with( ~ stringr::str_remove(., "[.].*"))
   }
+
+  print("Process complete")
+
+  return(data_clean)
+}
+
+#' Calls QB API 'run report' function
+#' @noRd
+run_report <- function(subdomain, user_token, table_id, report_id, agent,
+                       skip, top, paginate, pages = NULL, page_skip = 0){
+
+  # Needed when paginating
+  total_skip <- ifelse(page_skip == 0, skip, page_skip)
 
   # Build the API call
-  qb_url <- stringr::str_c("https://api.quickbase.com/v1/reports/",
-                  report_id,
-                  "/run?tableId=",
-                  table_id,
-                  skip,
-                  top)
+  qb_url <- paste0("https://api.quickbase.com/v1/reports/", report_id,
+                           "/run?tableId=", table_id,
+                           if(total_skip > 0){paste0("&skip=", total_skip)},
+                           if(top > 0){paste0("&top=", top)})
 
   # Deliver API call to QB via an HTTP request, store response
   data_raw <- httr::POST(qb_url,
@@ -85,42 +125,37 @@ qb_run <- function(subdomain, user_token, table_id, report_id, agent = NULL,
   # Stop the script if HTTP request fails
   httr::stop_for_status(data_raw)
 
+  print(data_raw)
+
   # Extract JSON payload from HTTP response and flatten
-  data_text <- jsonlite::fromJSON(httr::content(data_raw, as = "text"), flatten = TRUE)
-
-  # Alert user to unhandled pagination
-  meta <- data_text[[3]]
-  if(meta[["totalRecords"]] - meta[["numRecords"]] > 0){
-    print(paste0("Table ", table_id, ", Report ", report_id,
-                 " had more records than can be processed in a single call. ",
-                 "Pulled ", meta[["numRecords"]], " of ", meta[["totalRecords"]]))
-  }
-
-  # Prepare field labels for renaming values object
-  data_fields <- tibble::as_tibble(data_text[[2]]) %>%
-    dplyr::mutate(id = as.character(id),
-                  label_type = stringr::str_c(label, type, sep =".")) %>%
-    dplyr::arrange(id)
-
-  # Multi-dimensional fields to drop by suffix
-  drop_me <- c(".version", ".id", ".name", ".userName")
-
   tryCatch(
-    data_clean <- tibble::as_tibble(data_text[[1]]) %>%
-      dplyr::select(-dplyr::contains(drop_me)) %>%
-      dplyr::rename_with( ~ data_fields$label_type) %>%
-      dplyr::mutate(dplyr::across(dplyr::matches("[.]multitext"), ~ lapply(., paste, collapse = "; "))) %>%
-      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ purrr::map(., "email"))) %>%
-      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ lapply(., paste, collapse = "; "))) %>%
-      dplyr::mutate(dplyr::across(dplyr::matches("[.]multiuser"), ~ unlist(.))),
+    data_text <- jsonlite::fromJSON(httr::content(data_raw, as = "text"), flatten = TRUE),
     error = function(e)
       stop("The report data could not be parsed.
-           Try removing fields with complex data types."))
+           This is likely due to special characters in text or rich-text fields.
+           Try removing fields containing non-standard characters from your
+           Quickbase report, such as &rsquo;"))
 
-  if(type_suffix == FALSE){
-    data_clean <- data_clean %>%
-      dplyr::rename_with( ~ stringr::str_remove(., "[.].*"))
+  new_page <- tibble::as_tibble(data_text[[1]])
+  meta <- data_text[[3]]
+
+  # Stack new page onto extracted pages
+  if(!is.null(pages)){
+    pages <- rbind(pages, new_page)
+  } else {
+    pages <- new_page
   }
 
-  return(data_clean)
+  # If not last page, recur
+  if(meta$totalRecords - skip > nrow(pages) & (nrow(pages) < top | top == 0) & paginate){
+
+    print("Dataset too large for single call, paginating...")
+
+    return(run_report(subdomain, user_token, table_id, report_id, agent,
+                      skip, top, paginate, pages, skip + nrow(pages)))
+
+  } else {
+    return(list("data" = pages, "fields" = data_text[[2]]))
+  }
+
 }
--- README.md
+++ README.md
@@ -28,8 +28,16 @@ library(qbr)
 my_tibble <- qb_run(subdomain = "bhi",
     user_token = keyring::key_get("qb_example"),
     table_id = "bn9d8iesz",
-    report_id = "1",
-    flat = T)
+    report_id = "1")
+#> [1] "Calling Quickbase API"
+#> Response [https://api.quickbase.com/v1/reports/1/run?tableId=bn9d8iesz]
+#>   Date: 2022-05-12 13:58
+#>   Status: 200
+#>   Content-Type: application/json; charset=UTF-8
+#>   Size: 3.69 kB
+#> 
+#> [1] "Cleaning extracted data"
+#> [1] "Process complete"
 ```
 
 You’ll still need to render `README.Rmd` regularly, to keep `README.md`
